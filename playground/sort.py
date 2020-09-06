478ddd9f1afea34ee763faec25515732aa6f38a3
Thilaknath Ashok Kumar <thilaknath.ashok.kumar@sap.com>
2020-09-06 15:07:38 -0400

    final project scripts

---
+++ playground/sort.py
@@ -0,0 +1,4 @@
+numbers = [2,7,5,4,3,0]
+numbers.sort()
+print(numbers)
+
---
+++ scripts/run.py
@@ -0,0 +1,14 @@
+from machine_user import current_users, generate_report
+from event import Event
+
+events = [
+    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
+    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
+    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
+    Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
+    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
+    Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
+]
+
+users = current_users(events)
+print(users)
